a9298db1ee2dee327776dabd498e072ab5be327a
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-06-25 11:31:22 -0600

    Initial Commit

---
+++ SaveLink_1/SaveLink_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLink_1App.swift
+//  SaveLink_1
+//
+//  Created by Oscar Valdes on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SaveLink_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
